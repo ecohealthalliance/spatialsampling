bd37618396dcf52881304e5c7d8b7e9afc489237
Ernest Guevarra <ernest@guevarra.io>
2021-01-22 20:21:38 +0000

    change global setup

--- global.R
+++ global.R
@@ -20,7 +20,8 @@ library(magrittr)
 library(dplyr)
 
 ## Authenticate with Mapbox ----------------------------------------------------
-mapboxapi::mb_access_token(token = "pk.eyJ1IjoiZXJuZXN0Z3VldmFycmEiLCJhIjoiY2trN2M1NG41MGNtdzJwcXdwenhxa3d1ZyJ9.lCsz_t1SeHdp2aZW16SDdg")
+mapboxapi::mb_access_token(token = "pk.eyJ1IjoiZXJuZXN0Z3VldmFycmEiLCJhIjoiY2trN2M1NG41MGNtdzJwcXdwenhxa3d1ZyJ9.lCsz_t1SeHdp2aZW16SDdg",
+                           install = TRUE)
 
 ## Mapbox styles
 leShine   <- "ckk7gbtu20d9817qu6i8hbxet"
