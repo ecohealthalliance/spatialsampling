0d1e20566984262c95d858563b288e10a1ca3302
Ernest Guevarra <ernest@guevarra.io>
2021-01-22 06:44:26 +0000

    add Shiny folder

---
+++ spatialsampling/global.R
@@ -0,0 +1,29 @@
+## Libraries -------------------------------------------------------------------
+
+## Spatial packages
+library(rgdal)
+library(rgeos)
+library(raster)
+library(sf)
+library(mapboxapi)
+library(mapdeck)
+library(leaflet)
+library(spatialsampler)
+
+## Shiny packages
+library(shiny)
+library(shinydashboard)
+
+## Utilities and data wrangling
+library(magrittr)
+library(dplyr)
+
+## Authenticate with Mapbox ----------------------------------------------------
+mapboxapi::mb_access_token(token = Sys.getenv("MAPBOX_PUBLIC_TOKEN"))
+
+## Mapbox styles
+leShine   <- "ckk7gbtu20d9817qu6i8hbxet"
+decimal   <- "ckk7gdtbw0d3a17lf2hnuky50"
+satellite <- "ckk7geu550d8k17l1t39f1t43"
+standard  <- "ckk7geic50d9417juktz7ajet"
+northstar <- "ckk7iv17e0fnn17lt6cvi9ppz"
