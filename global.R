c500306f2d95ce0418ea1ab639fff6ccd8b0b4fc
Ernest Guevarra <ernest@guevarra.io>
2021-01-24 15:38:44 +0000

    make general non-specific changes

--- global.R
+++ global.R
@@ -18,7 +18,6 @@ library(spatialsampler)  ## spatial sampling functions
 
 ## Shiny packages
 library(shiny)
-library(shinydashboard)
 library(shinyFiles)      ## ui for selecting and uploading directories in Shiny
 library(colourpicker)
 library(shinyjs)
@@ -37,8 +36,11 @@ library(openxlsx)
 mapboxapi::mb_access_token(token = Sys.getenv("MAPBOX_PUBLIC_TOKEN"))
 
 ## Mapbox styles
+moonlight <- "cj3nban30001z2rpahc10c9ef"
 leShine   <- "ckk7gbtu20d9817qu6i8hbxet"
 decimal   <- "ckk7gdtbw0d3a17lf2hnuky50"
 satellite <- "ckk7geu550d8k17l1t39f1t43"
 standard  <- "ckk7geic50d9417juktz7ajet"
 northstar <- "ckk7iv17e0fnn17lt6cvi9ppz"
+terminal  <- "cj6g0tzbd30kc2sph2wyh666m"
+
